65b6587259e2780da0a1b8fc59242e4b1a1db9c5
Clayton Jones <52557433+clayton-jones@users.noreply.github.com>
2021-04-06 17:08:28 -0700

    Delete build directory

--- build/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Sorter_1 = require("./Sorter");
-var NumbersCollection_1 = require("./NumbersCollection");
-var numbers = new NumbersCollection_1.NumbersCollection([1, 4, -1, -20, 32, 10]);
-var sorter = new Sorter_1.Sorter(numbers);
-sorter.sort();
-console.log(numbers.data);
